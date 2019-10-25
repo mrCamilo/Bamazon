f32be55ad628d34da42d0c9aeab19ff3f37c0987
Manuel Camilo <mcamilo@protonmail.com>
2019-10-25 15:11:30 -0400

    added mock data to the sql file

--- app.js
+++ app.js
@@ -1,3 +1,50 @@
 // These are the node packages we need
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+// create the connection to sql here
+var conn = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "lolwut123",
+    database: "greatBay_DB"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+});
+
+function start() {
+    // First, display all of the items here
+    console.log("WELCOME TO THE BAMAZON RECORD STORE!!");
+
+    // Using inquirer, ask what item the user would like to buy
+    inquirer
+    .prompt({
+      name: "postOrBid",
+      type: "list",
+      message: "Would you like to [POST] an auction or [BID] on an auction?",
+      choices: ["POST", "BID", "EXIT"]
+    })
+    .then(function(answer) {
+      // based on their answer, either call the bid or the post functions
+      if (answer.postOrBid === "POST") {
+        postAuction();
+      }
+      else if(answer.postOrBid === "BID") {
+        bidAuction();
+      } else{
+        connection.end();
+      }
+    });
+}
---
+++ bamazonSchema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE table products( 
+    id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(45) NOT NULL,
+    price INT,
+    stock_quantity INT,
+    PRIMARY KEY (id)
+);
+
+--  Populate this database with some products
+Select * FROM products;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Tims", "clothing", 100, 15),
+	   ("JESUS IS KING - VINYL", "Music", 30, 100),
+       ("Black Air Forces", "clothing", 70, 22),
+       ("MLB 19 The Show", "videogames", 30, 41),
+       ("Modern Warfare", "videogames", 59.99, 10),
+       ("Dragon Quest XI", "videogames", 59.99, 6),
+       ("Confessions of a Mask - Yukio Mishima", "books", 12, 0),
+       ("Berserk - Volume 23", "books", 14, 2),
+       ("EXPLOSIVE CALISTHENICS", "books", 23, 7),
+       ("Taxi Driver - bluray", "movies", 20, 6)
