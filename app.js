2f74f7d126f40f7773a4db1b46c941023ed80fc8
Manuel Camilo <mcamilo@protonmail.com>
2019-10-28 13:04:15 -0400

    added password prompt

--- app.js
+++ app.js
@@ -2,50 +2,96 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var chalk = require("chalk"); // this just makes everything pretty
+var Password = require("./password");
+new Password().getPassword(loginCallback);
 
-// create the connection to sql here
-var connection = mysql.createConnection({
-  host: "localhost",
+function loginCallback(password) {
+  console.log("loginCallBack");
 
-  // Your port; if not 3306
-  port: 3306,
+  // create the connection to sql here
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: password,
+    database: "bamazon_db"
+  });
 
-  // Your username
-  user: "root",
+  
+  // variables
+  var howMany = 0;
+  var product = "";
 
-  // Your password
-  password: "lolwut123",
-  database: "bamazon_db"
-});
-
-// connect to the mysql server and sql database
-connection.connect(function (err) {
-  if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  start();
-});
-
-function start() {
-  // First, display all of the items here and welcome the customer to my store
-  console.log(chalk.blue("WELCOME TO THE BAMAZON STORE!!"));
-  console.log(chalk.green("This is what we have for sale..."));
-  connection.query("SELECT * FROM products", function (err, results) {
-    var theItems = [];
+  // connect to the mysql server and sql database
+  connection.connect(function (err) {
     if (err) throw err;
-    for (var i = 0; i < results.length; i++) {
-      theItems.push(results[i].product_name);
-      console.log(chalk.gray(theItems[i]));
-    }
-    // After that, we need to ask the user what they want to buy with a prompt
-    console.log("\n\n");
+    // run the start function after the connection is made to prompt the user
+    start();
+  });
 
-    inquirer
-      .prompt({
-        name: "postOrBid",
-        type: "list",
-        message: "What item would you like to buy?",
-        choices: theItems
-      })
-    connection.end();
-  })
+  function start() {
+    // First, display all of the items here and welcome the customer to my store
+    console.log(chalk.blue("WELCOME TO THE BAMAZON STORE!!"));
+    console.log(chalk.green("This is what we have for sale..."));
+    connection.query("SELECT * FROM products", function (err, results) {
+      var theItems = [];
+      if (err) throw err;
+      // for loop to push all the item names from mySQL into an array and log them
+      for (var i = 0; i < results.length; i++) {
+        theItems.push(results[i].product_name + chalk.blue(" | ID: " + results[i].id + chalk.red(") | Quantity: " + results[i].stock_quantity) + " | Price: $" + results[i].price));
+        console.log(chalk.red(theItems[i]));
+      }
+
+      // After that, we need to ask the user what they want to buy with a prompt
+      console.log("\n\n");
+      inquirer
+        .prompt([
+          {
+            name: "purchaseProduct",
+            type: "input",
+            message: "What product would you like to purchase? Please input the product's ID below:\n",
+            validate: function (value) {
+              if (isNaN(value) === false) {
+                return true;
+              }
+              return false;
+            }
+          },
+          {
+            name: "howMany",
+            type: "input",
+            message: "How many would you like to purchase? Please input the quantity below:\n"
+          }
+        ]).then(answer => {
+          // save the user responses
+          howMany = answer.howMany;
+          product = answer.purchaseProduct;
+          console.log("\n----------------------------------------------------\n");
+          buy();
+        });
+    })
+  }
+
+
+  function buy() {
+    connection.query('Select * FROM products WHERE id = ' + product, function (err, connection) {
+      //error
+      if (err) throw err;
+
+      // Only update the mysql databse if the user picks a valid quantity
+      if (howMany > connection[0].stock_quantity) {
+        console.log("We don't have sufficent " + connection[0].product_name + " in stock.");
+      }
+      else {
+        var price = connection[0].price * howMany;
+        console.log("Amount for your order of " + connection[0].product_name + ": " + "$" + price);
+        //("UPDATE products SET stock_quantity = stock_quantity - " + howMany + "WHERE item_id = " + product);
+        connection.query("UPDATE products SET stock_quantity = stock_quantity - " + howMany + "WHERE id = " + product);
+        // UPDATE products SET stock_quantity = stock_quantity - 1  WHERE id =  124;
+        console.log(chalk.magenta("Thanks for your purchase!! Have a great day :)"))
+        console.log(chalk.magenta("Updating marketplace...\n\n"));
+      };
+      start();
+    });
+  };
 }
--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE bamazon_db;
 USE bamazon_db;
 
 CREATE table products( 
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT,
     product_name VARCHAR(100) NOT NULL,
     department_name VARCHAR(45) NOT NULL,
     price INT,
@@ -15,15 +15,16 @@ CREATE table products(
 --  Populate this database with some products
 Select * FROM products;
 
-INSERT INTO products (product_name, department_name, price, stock_quantity) 
-VALUES ("Tims", "clothing", 100, 15),
-	   ("JESUS IS KING - VINYL", "Music", 30, 100),
-       ("Black Air Forces", "clothing", 70, 22),
-       ("MLB 19 The Show", "videogames", 30, 41),
-       ("Modern Warfare", "videogames", 59.99, 10),
-       ("Dragon Quest XI", "videogames", 59.99, 6),
-       ("Confessions of a Mask - Yukio Mishima", "books", 12, 0),
-       ("Berserk - Volume 23", "books", 14, 2),
-       ("EXPLOSIVE CALISTHENICS", "books", 23, 7),
-       ("Taxi Driver - bluray", "movies", 20, 6)
-
+INSERT INTO products (id, product_name, department_name, price, stock_quantity) 
+VALUES (124, "Tims", "clothing", 100, 15),
+	   (1518, "JESUS IS KING - VINYL", "Music", 30, 100),
+       (36, "Black Air Forces", "clothing", 70, 22),
+       (1317, "MLB 19 The Show", "videogames", 30, 41),
+       (1428, "Modern Warfare", "videogames", 59.99, 10),
+       (12, "Dragon Quest XI", "videogames", 59.99, 6),
+       (126, "Confessions of a Mask by Yukio Mishima", "books", 12, 0),
+       (1997, "Berserk - Volume 23", "books", 14, 2),
+       (630, "EXPLOSIVE CALISTHENICS", "books", 23, 7),
+       (1350, "Taxi Driver - bluray", "movies", 20, 6);
+       
+UPDATE products SET stock_quantity = stock_quantity - 1  WHERE id =  124;
